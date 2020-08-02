d54fc45043e84dd0938877e09e725e8c2460ef29
Robson Santos <robson.eng@hotmail.com>
2020-08-02 18:46:02 -0300

    First Commit

---
+++ all_check.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def check_reboot():
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+
+main()
